535ddb7ed7f25b419fbd536f87d42e929e85ad12
westleyaaj <72582341+westleyaaj@users.noreply.github.com>
2025-03-24 10:47:53 -0700

    Update menus.h bugfix

--- sysmodules/rosalina/include/menus.h
+++ sysmodules/rosalina/include/menus.h
@@ -38,6 +38,9 @@ void RosalinaMenu_ProcessList(void);
 void RosalinaMenu_SaveSettings(void);
 void RosalinaMenu_Cheats(void);
 
+void RosalinaMenu_PowerOff(void);
+void RosalinaMenu_Reboot(void);
+void RosalinaMenu_Secret(void);
 void RosalinaMenu_PowerOffOrReboot(void);
 
 void RosalinaMenu_ShowSystemInfo();
